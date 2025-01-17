6789f7c669dff807c3d8620d6f7fd15b11b0a6c3
Dzudzok <mateuszdurczok01@gmail.com>
2025-01-17 07:57:37 +0100

    Dodano dynamiczne ustawianie portu (5010 lokalnie, 8080 na Render)

--- app.py
+++ app.py
@@ -164,5 +164,6 @@ def process_file():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    import os
+    port = int(os.environ.get('PORT', 5010))  # Domyślnie 5010, ale na serwerze Render automatycznie ustawi 8080
     app.run(host='0.0.0.0', port=port)
